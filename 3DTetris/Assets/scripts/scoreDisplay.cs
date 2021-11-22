242dcb77af60918e2cfef02adac9daf5f42d4803
JacksonDonaldson <66980702+JacksonDonaldson@users.noreply.github.com>
2021-11-21 20:06:04 -0500

    made ui work with game

--- 3DTetris/Assets/scripts/scoreDisplay.cs
+++ 3DTetris/Assets/scripts/scoreDisplay.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class scoreDisplay : MonoBehaviour
 {
     public player p;
-    public Text Score;
+    private TextMeshProUGUI Score;
 
-
+    void Start()
+    {
+        Score = gameObject.GetComponent(typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+    }
     // Update is called once per frame
     void Update()
     {
-        Score.text = "SCORE: " + p.score;
+        Score.text = "Score: " + p.score;
     }
 }
